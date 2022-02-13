3169ba64da2bc40b60b0ac7733c3368958a2f598
Cardoso Assuncao <cardosoassuncao@gmail.com>
2022-02-13 14:29:40 -0400

    corrigidos alguns detalhes...

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function checksExistsUserAccount(request, response, next) {
   const { username } = request.headers
 	const user = users.find(user => user.username === username)
 	if (!user){
-		response.status(400).json({ error: `${username} not found!`})
+		response.status(404).json({ error: `${username} not found!`})
 	}
 	request.user = user
 	return next()
@@ -33,8 +33,7 @@ app.post('/users', (request, response) => {
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
   const { user } = request
-	return response.status(201).json(user.todos)
-	
+	return response.json(user.todos)     
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
@@ -57,13 +56,13 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
 	const { title, deadline } = request.body
 	const { user } = request
 	const { id } = request.params
-	let todo = user.todos.find(todo => todo.id === id)
+	const todo = user.todos.find(todo => todo.id === id)
 	if(!todo){
 		return response.status(404).json({error: `${id} not found!`})
 	}
 	todo.title = title
 	todo.deadline = new Date(deadline)
-	return response.status(201).json(todo)
+	return response.json(todo)
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
@@ -75,7 +74,7 @@ app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
 		return response.status(404).json({error: `${id} not found!`})
 	}
 	todo.done = true
-	return response.status(201).json(todo)
+	return response.json(todo)
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
@@ -87,7 +86,7 @@ app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
 		return response.status(404).json({error: `${id} not found!`})
 	}
 	user.todos.splice(todo, 1)
-	return response.status(204).json(todo)
+	return response.status(204).json()
 
 });
 
